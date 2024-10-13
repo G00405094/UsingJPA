5f25b289dcc96d98344111fd9da69c842b691292
G00405094 <G00405094@atu.ie>
2024-10-13 23:50:11 +0100

    Homework

--- src/main/java/ie/atu/week4/jpa/ProductController.java
+++ src/main/java/ie/atu/week4/jpa/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.week4.jpa;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    private List<Product> productList = new ArrayList<>();
+
     private ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -18,48 +19,23 @@ public class ProductController {
 
     @GetMapping("/getProducts")
     public List<Product> getProducts() {
-        return productList;
+        return productService.getProducts();
     }
 
     @PostMapping("/addProduct")
-    public ResponseEntity<List> addProduct(@RequestBody Product product) {
-        productList = productService.add(product);
-        //productList.add(product);
-        return ResponseEntity.ok(productList);
+    public Product addTask(@Valid @RequestBody Product product) {
+        productService.addProduct(product);
+        return product;
     }
 
-    private Product findProductById(int id) {
-        for (Product product : productList) {
-            if (product.getProductCode() == id) {
-                return product;
-            }
-        }
-        return null;
+    @DeleteMapping("/remove/{id}")
+    public void removeProduct(@Valid @PathVariable Long id) {
+        productService.removeProduct(id);
     }
 
-    @PutMapping("/updateProduct/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            existingProduct.setProductName(updatedProduct.getProductName());
-            existingProduct.setProductDescription(updatedProduct.getProductDescription());
-            existingProduct.setProductPrice(updatedProduct.getProductPrice());
-            return ResponseEntity.ok(existingProduct);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("/deleteProduct/{id}")
-    public ResponseEntity<List<Product>> deleteProduct(@PathVariable int id) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            productList.remove(existingProduct);
-            return ResponseEntity.ok(productList);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@Valid @PathVariable Long id, @Valid @RequestBody Product product) {
+        productService.updateProduct(id, product);
+        return product;
     }
 }
